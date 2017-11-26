6771dd2f8b7be966fbb525fdbfcf152689c7fdb6
james mullinix <james.mullinix@gmail.com>
2017-11-25 21:04:10 -0800

    jam -- rotation possible for galil. need to write routine to test for completion.

--- RT_Communicator.py
+++ RT_Communicator.py
@@ -3,25 +3,32 @@ import time
 
 class RT_Communicator:
 #	def __init__(self):
-#		print "DMM Object Initialized\n"
+#		print "DMM Object Initialized\r\n"
 
-	def turn_galil(self,ser,AC=25600,DC=25600,SP=151200000,theta=0)
-		ser.write('AC='+AC+'\n')
-		ser.write('DC='+DC+'\n')
-		ser.write('SP='+SP+'\n')
-		ser.write('PRA='+theta+'\n')
-		ser.write('BGA\n')
-		#return ser.readline()
+        def turn_galil(self,ser,AC=256000,DC=256000,SP=15120,theta=0):
+                writestr='ACA='+str(AC)+\
+                        ';DCA='+str(DC)+\
+                        ';SPA='+str(SP)+\
+                        ';PRA='+str(theta)+\
+                        ';BG A\r\n'
+                ser.write(writestr)
+#                ser.write('TC1\r\n')
+                ser.flush()
+		return ser.readline()
 	
-	def init_galil(self,PortIn='COM1', BaudIn=19200, TOIn=1):
+	def init_galil(self,PortIn='COM1', BaudIn=19200, TOIn=5):
 		ser=serial.Serial(port=PortIn,baudrate=BaudIn, timeout=TOIn)
-		ser.write('SHA\n')
+		ser.write('SH A\r\n')
+                ser.flush()
+                print ser
 		return ser
 	
 	def test_galil(self):
 		ser=self.init_galil()
 		for i in xrange(10):
-			print self.turn_galil(ser=ser,theta=10*(i+1))
+			print self.turn_galil(ser=ser,theta=1000*(i+1))
 			time.sleep(2)
+
+                ser.close()
 
 		
