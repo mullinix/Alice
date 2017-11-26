b9e710d2ff1daca1d76dafce247f25b2d7b5fb47
james mullinix <james.mullinix@gmail.com>
2017-11-26 14:01:35 -0800

    jam -- added experiment controller

---
+++ run_exp.py
@@ -0,0 +1,35 @@
+from DMM_Communicator import DMM_Communicator
+from FOG_Communicator import FOG_Communicator
+from RT_Communicator import RT_Communicator
+import numpy as np
+import time
+
+num_samples = 30
+shift_degs = 15
+num_shifts = 180/shift_degs
+
+data=np.zeros((num_samples*num_shifts,3))
+
+dmm = DMM_Communicator()
+dmmser = dmm.init_agilent()
+
+rt = RT_Communicator()
+rtser = rt.init_galil()
+
+fog = FOG_Communicator()
+fogser = fog.init_crossbow()
+
+degs=0
+for shift in xrange(num_shifts):
+    for sample in xrange(num_samples):
+        idx = shift*num_samples+sample
+        volts = dmm.read_agilent(dmmser)
+        counts = fog.read_crossbow(fogser)
+        data[idx,]=[degs,volts,counts]
+    rt.turn_galil(ser=rtser,theta=shift_degs)
+    degs+=shift_degs
+    time.sleep(5)
+
+print data
+
+np.savetxt("results.csv",data,delimeter=",")
